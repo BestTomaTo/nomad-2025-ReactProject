58e943dd897487c7bbf4cc9c91b76d5c66edccc5
spacejin2000 <spacejin2000@naver.com>
2025-01-07 15:46:58 +0900

    react effect/useEffect 공부

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setCount] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setCount((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(() => {
+    console.log("call the API");
+  }, []);
+  useEffect(() => {
+    console.log("i run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("i run when 'counter' changes.");
+  }, [counter]);
   return (
     <div>
-      <h1 className={styles.title}>Welcome Back!</h1>
-      <Button text={"Continue"} />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search Here..."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Press the button</button>
     </div>
   );
 }
